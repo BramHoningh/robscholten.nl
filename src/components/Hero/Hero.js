1e2d602aa35f1dceccdaf0d06c5787854fd54d0c
bram.honingh <bram.honingh@mediamonks.com>
2018-12-17 17:11:20 +0100

    Add webp image format

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,3 +1,129 @@
 export default {
   name: 'Hero',
+  data() {
+    return {
+      rows: [
+        {
+          images: [
+            {
+              webp: 'db-asset-1@1x.webp',
+              png: 'db-asset-1.png',
+            },
+            {
+              webp: 'db-asset-2@1x.webp',
+              png: 'db-asset-2.png',
+            },
+            {
+              webp: 'db-asset-3@1x.webp',
+              png: 'db-asset-3.png',
+            },
+            {
+              webp: 'db-asset-4@1x.webp',
+              png: 'db-asset-4.png',
+            },
+            {
+              webp: 'db-asset-5@1x.webp',
+              png: 'db-asset-5.png',
+            },
+          ],
+        },
+        {
+          images: [
+            {
+              webp: 'db-asset-6@1x.webp',
+              png: 'db-asset-6.png',
+            },
+            {
+              webp: 'db-asset-7@1x.webp',
+              png: 'db-asset-7.png',
+            },
+            {
+              webp: 'db-asset-8@1x.webp',
+              png: 'db-asset-8.png',
+            },
+            {
+              webp: 'db-asset-9@1x.webp',
+              png: 'db-asset-9.png',
+            },
+            {
+              webp: 'db-asset-10@1x.webp',
+              png: 'db-asset-10.png',
+            },
+          ],
+        },
+        {
+          images: [
+            {
+              webp: 'db-asset-11@1x.webp',
+              png: 'db-asset-11.png',
+            },
+            {
+              webp: 'db-asset-12@1x.webp',
+              png: 'db-asset-12.png',
+            },
+            {
+              webp: 'db-asset-13@1x.webp',
+              png: 'db-asset-13.png',
+            },
+            {
+              webp: 'db-asset-14@1x.webp',
+              png: 'db-asset-14.png',
+            },
+            {
+              webp: 'db-asset-14@1x.webp',
+              png: 'db-asset-14.png',
+            },
+          ],
+        },
+        {
+          images: [
+            {
+              webp: 'db-asset-15@1x.webp',
+              png: 'db-asset-15.png',
+            },
+            {
+              webp: 'db-asset-16@1x.webp',
+              png: 'db-asset-16.png',
+            },
+            {
+              webp: 'db-asset-17@1x.webp',
+              png: 'db-asset-17.png',
+            },
+            {
+              webp: 'db-asset-18@1x.webp',
+              png: 'db-asset-18.png',
+            },
+            {
+              webp: 'db-asset-19@1x.webp',
+              png: 'db-asset-19.png',
+            },
+          ],
+        },
+        {
+          images: [
+            {
+              webp: 'db-asset-20@1x.webp',
+              png: 'db-asset-20.png',
+            },
+            {
+              webp: 'db-asset-21@1x.webp',
+              png: 'db-asset-21.png',
+            },
+            {
+              webp: 'db-asset-22@1x.webp',
+              png: 'db-asset-22.png',
+            },
+            {
+              webp: 'db-asset-23@1x.webp',
+              png: 'db-asset-23.png',
+            },
+            {
+              webp: 'db-asset-24@1x.webp',
+              png: 'db-asset-24.png',
+            },
+          ],
+        },
+      ],
+    };
+  },
 };
--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -11,40 +11,13 @@
 
     <div class="hero-images">
       <div class="overlay"></div>
-      <div class="row">
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-1.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-2.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-3.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-4.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-5.png"></div>
-      </div>
-      <div class="row">
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-6.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-7.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-8.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-9.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-10.png"></div>
-      </div>
-      <div class="row">
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-11.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-12.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-13.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-14.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-15.png"></div>
-      </div>
-      <div class="row">
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-16.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-17.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-18.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-19.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-20.png"></div>
-      </div>
-      <div class="row">
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-21.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-22.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-23.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-24.png"></div>
-        <div class="image"><img src="../../assets/images/db-assets/db-asset-25.png"></div>
+      <div class="row" v-for="(row, index) in rows" :key="index">
+        <div class="image" v-for="(image, i) in row.images" :key="index + i">
+          <picture>
+            <source :srcset="require(`@/assets/images/db-assets/${image.webp}`)" type="image/webp">
+            <img :src="require(`@/assets/images/db-assets/${image.png}`)">
+          </picture>
+        </div>
       </div>
     </div>
   </div>
