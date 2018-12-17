ad380f43b918d711aad02e69c85865e24c4ba910
Pixeldenker <bram@pixeldenker.nl>
2018-12-17 10:41:27 +0100

    Add animation:

--- src/components/Hero/Hero.vue
+++ src/components/Hero/Hero.vue
@@ -5,13 +5,16 @@
   <div class="hero">
     <div class="hero-inner">
       <h1>I Love Pixels</h1>
-      <p>Hey, I’m Rob Scholten. A screen designer From the netherlands. Currently I’m a designer at Strakzat and running Pixeldenker in my free time.</p>
+      <p>Hey, I’m Rob Scholten. A screen designer from the Netherlands. Currently I’m a designer at Strakzat and running Pixeldenker in my free time.</p>
       <a href="#" class="btn">View Portfolio</a>
     </div>
 
     <div class="hero-images">
-      <div class="image">
-        <img src="../../assets/images/db-assets/db-asset 01.png" alt="">
+      <div class="overlay"></div>
+      <div class="row" v-for="i in 5" :key="i">
+        <div class="image" v-for="index in 5" :key="index">
+          <img src="../../assets/images/db-assets/db-asset-1.png">
+        </div>
       </div>
     </div>
   </div>
